be4492b8953ad75060bf4ff483705662c344fd6e
ywcheong <tencise@gmail.com>
2024-11-30 19:06:08 +0900

    Init: 데이터베이스 공부 목적 Repository

---
+++ src/test/java/com/ywcheong/test/simpledatabase/SimpleDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywcheong.test.simpledatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
